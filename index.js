5e0cf624c56130d86503f603fd6ab4be403453d7
izabela1337 <54004218+izabela1337@users.noreply.github.com>
2023-06-30 01:10:53 +0200

    image supprt

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
@@ -5,10 +6,15 @@
  */
 module.exports = (app) => {
   // Your code here
-  const headers = {
+  const openaiHeaders = {
     'Content-Type' : 'application/json',
     'Authorization' : `Bearer ${process.env.OPENAI_API_KEY}`
   }
+  const getimgHeaders = {
+    'Content-Type' : 'application/json',
+    'Authorization' : `Bearer ${process.env.GETIMG_API_KEY}`
+  }
+
   app.log.info("Yay, the app was loaded!");
 
   app.on("pull_request.opened", async (context) => {
@@ -28,18 +34,60 @@ module.exports = (app) => {
       ],
       "temperature": 0.7
     }
-    body = JSON.stringify(body)
-    let response = await fetch("https://api.openai.com/v1/chat/completions",
+    bodyJSON = JSON.stringify(body)
+    let responseReview = await fetch("https://api.openai.com/v1/chat/completions",
       {
-        headers: headers,
+        headers: openaiHeaders,
         method: "POST",
-        body: body
+        body: bodyJSON
       }
     )
-    let responseJson = await response.json()
+    let responseReviewJson = await responseReview.json()
+
+    body.messages[2] = {"role" : "assistant", "content" : "responseReviewJson.choices[0].message.content"}
+    body.messages[3] = {"role" : "system", "content" : "For the next message, use normal english."}
+    body.messages[4] = {"role" : "user", "content" : "write an image creation prompt about how you look while writing this review. Make sure to describe yourself as an anime girl. Keep it simple."}
+
+    bodyJSON = JSON.stringify(body)
+    let responseImagePrompt = await fetch("https://api.openai.com/v1/chat/completions",
+      {
+        headers: openaiHeaders,
+        method: 'POST',
+        body: bodyJSON
+      })
+    let responseImagePromptJson = await responseImagePrompt.json()
+
+    let imageCreationBody = {
+      "model": "eimis-anime-diffusion-v1-0",
+      "prompt": responseImagePromptJson.choices[0].message.content,
+      "negative_prompt": "Disfigured, cartoon, blurry",
+      "width": 512,
+      "height": 512,
+      "steps": 25,
+      "guidance": 7.5,
+      "seed": 42,
+      "scheduler": "dpmsolver++",
+      "output_format": "jpeg"
+    }
+    imageCreationBody = JSON.stringify(imageCreationBody)
+    let responseImage = await fetch("https://api.getimg.ai/v1/stable-diffusion/text-to-image",
+      {
+        headers: getimgHeaders,
+        method: 'POST',
+        body: imageCreationBody
+      }
+    )
+    let imageJson = await responseImage.json()
+
+    let imageBuff = Buffer.from(imageJson.image, 'base64')
+
+    fs.writeFileSync('/pictures/' + context.payload.pull_request.base.sha + '.jpg', imageBuff)
+
+    let commentBody = responseReviewJson.choices[0].message.content
+    commentBody = commentBody.concat("\n", `![img](https://kawaii-commenter.nordvik2077.xyz/pictures/${context.payload.pull_request.base.sha}.jpg)`)
 
     const prComment = context.issue({
-      body: responseJson.choices[0].message.content,
+      body: commentBody,
     });
     return context.octokit.issues.createComment(prComment);
   });
--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 docker build -t kawaii-commenter .
 docker stop kawaii-commenter
 docker rm kawaii-commenter
-docker run -d --name kawaii-commenter -p 3000:3000 -v $(pwd)/.env:/usr/src/app/.env kawaii-commenter
+docker run -d --name kawaii-commenter -p 3000:3000 -v $(pwd)/.env:/usr/src/app/.env -v /srv/kawaii-commenter/pictures:/pictures kawaii-commenter
