9ed0f066356238937021afdcf2e281d42581fc4f
izabela1337 <54004218+izabela1337@users.noreply.github.com>
2023-07-10 00:38:46 +0200

    Error handling

--- index.js
+++ index.js
@@ -11,26 +11,30 @@ module.exports = (app) => {
   app.log.info("Yay, the app was loaded!");
 
   app.on("pull_request.opened", async (context) => {
-    let diff = await context.octokit.rest.pulls.get({
-      owner: context.payload.repository.owner.login,
-      repo: context.payload.repository.name,
-      pull_number: context.payload.pull_request.number,
-      mediaType : {
-        format: "diff"
-      }
-    })
-    let responseReview = await openai.createReview(diff)
-    let responseImagePrompt = await openai.createImagePrompt(responseReview.choices)
-    let responseImage = await getimg.createImage(responseImagePrompt.choices[0].message.content)
-    utils.saveImage(responseImage, context.payload.pull_request.base.sha)
+    try {
+      let diff = await context.octokit.rest.pulls.get({
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        pull_number: context.payload.pull_request.number,
+        mediaType : {
+          format: "diff"
+        }
+      })
+      let responseReview = await openai.createReview(diff)
+      let responseImagePrompt = await openai.createImagePrompt(responseReview.choices)
+      let responseImage = await getimg.createImage(responseImagePrompt.choices[0].message.content)
+      utils.saveImage(responseImage, context.payload.pull_request.base.sha)
 
-    let commentBody = responseReview.choices[0].message.content
-    commentBody = commentBody.concat("\n", `![img](https://${process.env.DOMAIN}/pictures/${context.payload.pull_request.base.sha}.jpg)`)
+      let commentBody = responseReview.choices[0].message.content
+      commentBody = commentBody.concat("\n", `![img](https://${process.env.DOMAIN}/pictures/${context.payload.pull_request.base.sha}.jpg)`)
 
-    const prComment = context.issue({
-      body: commentBody,
-    });
-    return context.octokit.issues.createComment(prComment);
+      const prComment = context.issue({
+        body: commentBody,
+      });
+      return context.octokit.issues.createComment(prComment);
+    } catch (err) {
+      utils.returnErrorComment(context, err)
+    }
   });
 
   // For more information on building apps:
--- utils.js
+++ utils.js
@@ -11,6 +11,10 @@ class Utils {
         body: body
       }
     )
+    if (!response.ok){
+      console.log(await response.json())
+      throw new Error(`There was a problem contacting ${url}.`)
+    }
     return await response.json();
   }
   async fetchImage(url, headers, body) {
@@ -20,6 +24,15 @@ class Utils {
   saveImage(buffer, name) {
     fs.writeFileSync('/pictures/' + name + '.jpg', buffer)
   }
+  returnErrorComment(context, error) {
+    console.log(error)
+    let message = "Sowwy, something went oopsy doopsy and I could not complete a reqwwest for you 😭😭😭. If this occurs frequently, please contact app owner @izabela1337."
+    message = message.concat("\n", "The error was:", "\n", "```", error, "```")
+    let body = context.issue({
+      body : message
+    })
+    context.octokit.issues.createComment(body)
+  }
 }
 
 const utilsInstance = new Utils();
@@ -27,5 +40,6 @@ const utilsInstance = new Utils();
 module.exports = {
   fetchJson: utilsInstance.fetchJson,
   fetchImage: utilsInstance.fetchImage,
-  saveImage: utilsInstance.saveImage
+  saveImage: utilsInstance.saveImage,
+  returnErrorComment: utilsInstance.returnErrorComment
 }
